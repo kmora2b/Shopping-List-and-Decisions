3bf510c83196f2b5c9500e12ffd3d3b8b6b329e7
Kim <kmora2b@gmail.com>
2018-03-13 20:41:22 -0600

    Add files via upload
    Using ternary, switch, and regular if else statements.

--- lab9.java
+++ lab9.java
@@ -4,15 +4,17 @@ public class lab9 {
 	
 	public static void main (String args []) {
 		
+		Scanner scnr = new Scanner(System.in);
 		float UB = 0;
+		String yesNo;
 		
 		initialBudget(UB);
+		yesNo = menuChoice();
 		
-		do {
-		menuChoice();
-		}
+		Questions (yesNo);
 		
-		while (false);
+		
+		
 	}
 	
 	public static double initialBudget (double userBudget) {
@@ -26,14 +28,11 @@ public class lab9 {
 		
 	}
 	
-	public static boolean menuChoice () {
-		
+	public static String menuChoice () {
 		Scanner scnr = new Scanner(System.in);
 		int count = 0;
 		int promptUser;
-		
-		boolean again = true;
-		boolean end = false;
+		String yesNo = " ";
 		
 		do {
 		System.out.println("Please pick a menu choice below:"
@@ -49,34 +48,14 @@ public class lab9 {
 				double itemPrice = scnr.nextDouble();
 				
 				System.out.println("\nCan you comfortably afford it?");
-				String yn = scnr.next();
-				
-				if (yn.equalsIgnoreCase("yes")) {
-					System.out.println("Do you have an immediate use for it?");
-					yn = scnr.next();
-					
-					if (yn.equalsIgnoreCase("yes")) {
-						System.out.println("Do you have cash to buy it instead of using a credit card?");
-					}
-					
-					if (yn.equalsIgnoreCase("no")) {
-						System.out.println("You don't truly need this item");
-						System.out.println("DON'T BUY IT\n");
-						//return again;
-					}
-					
-				}
-				
-				else if ( yn.equalsIgnoreCase("no") ) {
-					System.out.println("Can you borrow it from a friend?");
-				}
-				break;
+				yesNo = scnr.next();
+				return yesNo;
 			
 			//FIXME
 			case 2:
 				System.out.printf("\nYour total expense amount is: "); //followed by the sum of the items purchased. 
 				System.out.printf("\nYour remaining balance is: "); //followed by the difference between the budget and the expenses. 
-				return end;
+				break;
 			
 				
 			default:
@@ -85,7 +64,7 @@ public class lab9 {
 				
 				if (count == 3) { 
 					System.out.println("The program has ended due to 3 wrong inputs.");
-					return end;
+					break;
 				}
 				
 			}
@@ -94,7 +73,7 @@ public class lab9 {
 		
 		while ( (promptUser != 1) && (promptUser != 2) && (count < 3) );
 	
-		return end;
+		return yesNo;
 		
 	}
 	
@@ -112,6 +91,110 @@ public class lab9 {
 		return remainBal;
 		
 	}
+	
+	public static void Questions (String yesNo) {
+		Scanner scnr = new Scanner(System.in);
+		int choice;
+		String tell = "";
+		String invalid = "invalid";
+		
+		String q1 = "Do you have an immediate use for it?";
+		String q2 = "Can you borrow it from a friend?";
+		String q3 = "Do you have cash to buy it instead of using a credit card?";
+		String q4 = "Can you put it on layaway?";
+		String q5 = "Do you own something that serves the same purpose?";
+		String q6 = "Could your money be better spent on meeting a need?";
+		String q7 = "Have you looked for sales, coupons, or generic versions?";
+		String q8 = "After all this, do you still want to buy it?";
+		String q9 = "You don't truly need this item.";
+		
+		
+		String error = "ERROR: Please input yes or no.";
+
+		
+		switch(yesNo.toLowerCase()) {
+			
+			case "yes":
+				System.out.println(q1);
+				yesNo = scnr.next();
+				
+				tell = yesNo.equalsIgnoreCase("yes")? q3: (yesNo.equalsIgnoreCase("no")? q9 + "\nDON'T BUY IT": error);
+				if (tell.equals(q3)) {
+					System.out.println(q3);
+					yesNo = scnr.next();
+				}
+				
+				tell = yesNo.equalsIgnoreCase("yes")? q5: (yesNo.equalsIgnoreCase("no")? q2: error);
+				if (tell.equals(q5)) {
+					System.out.println(q5);
+					yesNo = scnr.next();
+					tell = yesNo.equalsIgnoreCase("yes")? q9 + "\nDON'T BUY IT": (yesNo.equalsIgnoreCase("no")? q6: error);
+				}
+				
+				else if (tell.equals(q2)) {
+					System.out.println(q2);
+					yesNo = scnr.next();
+					tell = yesNo.equalsIgnoreCase("yes")? "\nASK TO BORROW IT": (yesNo.equalsIgnoreCase("no")? q4: error);
+				}
+				
+				
+				
+				if (tell.equals(q6)) {
+					System.out.println(q6);
+					yesNo = scnr.next();
+					tell = yesNo.equalsIgnoreCase("yes")? "\nGO HOME AND SAVE UP": (yesNo.equalsIgnoreCase("no")? q7: error);
+								
+						if (tell.equals(q7)) {
+							System.out.println(q7);
+							yesNo = scnr.next();
+							tell = yesNo.equalsIgnoreCase("yes")? q8: (yesNo.equalsIgnoreCase("no")? "\nCOMPARISON SHOP BEFORE BUYING": error);
+						}
+				}
+				
+				else if (tell.equals(q4)) {
+							System.out.println(q4);
+							yesNo = scnr.next();
+							tell = yesNo.equalsIgnoreCase("yes")? "\nPUT IT ON LAYAWAY": (yesNo.equalsIgnoreCase("no")? "\nGO HOME AND SAVE UP": error);
+				}
+				
+				
+				
+				if (tell.equals(q8)) {
+					System.out.println(q8);
+					yesNo = scnr.next();
+					tell = yesNo.equalsIgnoreCase("yes")? "\nBUY IT ALREADY": (yesNo.equalsIgnoreCase("no")? "\nDON'T BUY IT": error);
+				}
+				
+				System.out.println(tell);
+				break;
+			
+			case "no":
+				System.out.println(q1);
+				yesNo = scnr.next();
+				
+				
+				//return yesNo;
+				
+			default:
+				//return invalid;
+				
+			
+			
+			
+			
+			
+			
+		}
+
+		//return tell;
+		
+		//String a = yesNo.equalsIgnoreCase("yes")? "Do you have an immediate use four it?": "Can you borrow it from a friend?";
+		
+		
+		
+	}
+	
+	
 	
 	
 	
